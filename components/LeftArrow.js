818fd6ff0477d9662bb09fff179f00705abb9bc8
Ryan Bridglal <ryanjbcom@users.noreply.github.com>
2018-04-05 22:26:55 -0700

    add the on click event

--- components/LeftArrow.js
+++ components/LeftArrow.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react ';
 const LeftArrow = (props) => {
 
 	return (
-		<div className="backArrow">
+		<div onClick={props.previousSlide} className="backArrow">
 			<i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i>
 		</div>
 
