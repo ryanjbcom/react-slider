13bb34baef46c98548bdc0e08a108991b44de211
Ryan Bridglal <ryanjbcom@users.noreply.github.com>
2018-04-05 20:59:30 -0700

    Create Slider.js

---
+++ components/Slider.js
@@ -0,0 +1,19 @@
+/* Parent constructor */
+
+import React, { Component } from 'react';
+
+export default class Slider extends Component {
+	constructor(props) {
+	super(props);
+	
+	this.state = {}
+	}
+
+	render() {
+		return (
+			<div className="slider">
+			</div>
+		);
+	}
+
+}
