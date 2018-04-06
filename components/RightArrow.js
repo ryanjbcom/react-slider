a451e054826b7a3d112383f5ce9a37684671cfc9
Ryan Bridglal <ryanjbcom@users.noreply.github.com>
2018-04-05 22:13:56 -0700

    add the code

--- components/RightArrow.js
+++ components/RightArrow.js
@@ -1,1 +1,14 @@
+/* Right Arrow JS code */
 
+import React, { Component } from 'react' };
+
+const RightArrow = (props) => {
+	return (
+		<div className="nextArrow">
+			<i className=" fa fa-arrow-right fa-2x" aria-hidden="true"></i>
+		</div>
+	);
+
+}
+
+export default RightArrow;
