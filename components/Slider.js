23877c4432c2f6b78eaeab84620cf5fc52e2c96e
Ryan Bridglal <ryanjbcom@users.noreply.github.com>
2018-04-05 22:01:19 -0700

    Import the slides and add the counter to run through them

--- components/Slider.js
+++ components/Slider.js
@@ -1,17 +1,29 @@
 /* Parent constructor */
 
 import React, { Component } from 'react';
+import SlideOne from './SlideOne';
+import SlideTwo from './SlideTwo';
+import SlideThree from './SlideThree';
+
 
 export default class Slider extends Component {
 	constructor(props) {
 	super(props);
 	
-	this.state = {}
+		this.state = {
+			slideCount: 1 
+		}
 	}
 
 	render() {
 		return (
 			<div className="slider">
+
+			/* Slides */
+			{ this.state.slideCount === 1 ? <SlideOne /> : null }
+			{ this.state.slideCount === 2 ? <SlideTwo /> : null }
+			{ this.state.slideCount === 3 ? <SlideThree /> : null }
+
 			</div>
 		);
 	}
