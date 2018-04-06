a074f33ee07cb662c58c644961c9ac63bcfce6ea
Ryan Bridglal <ryanjbcom@users.noreply.github.com>
2018-04-05 22:25:59 -0700

    add clickevent to right arrow

--- components/RightArrow.js
+++ components/RightArrow.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react' };
 
 const RightArrow = (props) => {
 	return (
-		<div className="nextArrow">
+		<div onClick={props.nextSlide} className="nextArrow">
 			<i className=" fa fa-arrow-right fa-2x" aria-hidden="true"></i>
 		</div>
 	);
