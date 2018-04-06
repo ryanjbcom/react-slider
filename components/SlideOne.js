20f487c59e7acf7e897413599836ac5307f49af0
Ryan Bridglal <ryanjbcom@users.noreply.github.com>
2018-04-05 21:16:22 -0700

    add image background

--- components/SlideOne.js
+++ components/SlideOne.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 const SlideOne= (props) => {
-	return <div className="slide"></div>
+
+	let background = {
+		backgroundImage: 'url(image1.jpg)',
+		backgroundSizeL 'cover',
+		backgroundPosition: 'center'
+	}
+
+	return <div style={background} className="slide"></div>
 }
 
 export default SlideOne;
